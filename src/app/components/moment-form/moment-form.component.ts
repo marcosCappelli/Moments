dac27446d97883e9be5e569649e52a894869cb0a
Marcoscappelli <cappelli.m@gmail.com>
2022-08-22 21:32:01 -0300

    validação formulario

--- src/app/components/moment-form/moment-form.component.ts
+++ src/app/components/moment-form/moment-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-moment-form',
@@ -7,10 +8,34 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class MomentFormComponent implements OnInit {
   @Input() btnText!: string;
+
+  momentForm!: FormGroup;
   
   constructor() { }
 
   ngOnInit(): void {
+    this.momentForm = new FormGroup({
+      id: new FormControl(''),
+      title: new FormControl('', [Validators.required]),
+      description: new FormControl('', [Validators.required]),
+      Image: new FormControl(''),
+    });
+  }
+
+  get title() {
+    return this.momentForm.get('title')!;
+  }
+
+  get description() {
+    return this.momentForm.get('description')!;
+  }
+
+  submit() {
+    if (this.momentForm.invalid) {
+      return;
+    }
+    
+    console.log('Enviou o formulário');
   }
 
 }
