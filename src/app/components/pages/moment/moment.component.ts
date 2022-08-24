2ee877a31321178d94e23d41bf29021f20603f79
Marcoscappelli <cappelli.m@gmail.com>
2022-08-24 16:24:59 -0300

    CSS page moment

--- src/app/components/pages/moment/moment.component.ts
+++ src/app/components/pages/moment/moment.component.ts
@@ -5,6 +5,9 @@ import { MomentService } from 'src/app/services/moment.service';
 
 import { Moment } from 'src/app/Moment';
 
+import { environment } from 'src/environments/environment';
+
+import { faTimes, faEdit } from '@fortawesome/free-solid-svg-icons';
 
 
 @Component({
@@ -14,6 +17,10 @@ import { Moment } from 'src/app/Moment';
 })
 export class MomentComponent implements OnInit {
   moment?: Moment;
+  baseApiUrl = environment.baseApiUrl;
+
+  faTimes = faTimes;
+  faEdit= faEdit;
 
   constructor(private momentService: MomentService,
      private route: ActivatedRoute
